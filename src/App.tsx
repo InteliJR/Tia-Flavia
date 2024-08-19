b5189561ec81762bf367dd3198c6a6a4718d7971
MarcRuas <marco.peixoto@sou.inteli.edu.br>
2024-08-19 01:00:24 -0300

    feat: adiciona componente home

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Footer from './components/Footer'
 import Donate from './components/Donate'
 import { useNavigate } from 'react-router-dom'
 import AboutUs from './components/AboutUs'
+import Home from './components/Home'
 
 export default function App() {
   const navigate = useNavigate()
   return (
-    <div className='font-bold text-3xl' >
+    <div className='text-md' >
       <Navbar />
+      <Home/>
       <AboutUs />
       <Donate />
       <Footer />
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,8 +2,36 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      Ola
+    <div className='bg-[#EE8DB3] flex items-center justify-center p-6'>
+      <div className='flex flex-col py-20 px-10'>
+        <h2 className='font-bold text-4xl'>
+          O amor de Deus
+          <br />
+          cuidando das
+          <br />
+          crianças em todas
+          as horas
+        </h2>
+
+        <div className='flex items-center justify-left py-6'>
+          <div>
+            <button type='submit' className='bg-[#DB247C] rounded-full text-white py-2 px-6'>
+              Faça parte!
+            </button>
+          </div>
+
+          <div>
+            <p className='text-lg px-6'>
+              +5.000 crianças impactadas
+              <br />
+              com a ajuda da comunidade.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div >
+        <img src='/image_home.png' />
+      </div>
     </div>
   )
 }
