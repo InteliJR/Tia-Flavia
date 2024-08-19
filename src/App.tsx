fef0bf37183c75e79b49640c3ac22b9861ed13ea
MarcRuas <marco.peixoto@sou.inteli.edu.br>
2024-08-19 00:09:52 -0300

    feat: adiciona componente about us

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Donate from './components/Donate'
 import { useNavigate } from 'react-router-dom'
+import AboutUs from './components/AboutUs'
 
 export default function App() {
   const navigate = useNavigate()
   return (
     <div className='font-bold text-3xl' >
       <Navbar />
+      <AboutUs />
       <Donate />
       <Footer />
     </div>
